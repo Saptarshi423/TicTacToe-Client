50f50e3a618f7b2764174d8362049146a4c2470d
saptarshi.kattyayan <saptarshi.kattyayan@nordson.com>
2024-07-06 11:51:57 +0530

    added ability for user to join a particular room.

--- constants.ts
+++ constants.ts
@@ -8,14 +8,18 @@ export interface ClientToServerEvents {
   broadcastClick : (res:{input:input, turn:string}) => void;
   broadcast_winEvent : (res:{winner: string}) => void;
   broadcast_resetEvent : ()=>void
+  test : (res:{msg:string}) => void,
+  broadcast_socketToRoom_connected : (res:{msg:string, user_id:number})=>void
+  broadcast_user_overflow : ()=>void
 }
 
 
 //Client to server side.
 export interface ServerToClientEvents {
-  win_Event : (req:{winner:string})=>void;
-  reset_Event : ()=>void;
+  join_room_Event : (req : {roomNumber:number}) => void
+  win_Event : (req:{winner:string, callback : ()=>void})=>void;
   clicked: (req:{input:input, turn:string}) => void;
+  reset_Event : (req:input)=>void;
 }
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { io, Socket } from 'socket.io-client';
-//import { ClientToServerEvents, ServerToClientEvents } from '../constants';
-
-
-// //Server to client side.
-interface ClientToServerEvents {
-  broadcastClick : (res:{input:input, turn:string}) => void;
-  broadcast_winEvent : (res:{winner: string}) => void;
-  broadcast_resetEvent : (res:input)=>void
-}
-
-
-//Client to server side.
-interface ServerToClientEvents {
-  win_Event : (req:{winner:string})=>void;
-  reset_Event : (req:input)=>void;
-  clicked: (req:{input:input, turn:string}) => void;
-}
-
+import { ClientToServerEvents, ServerToClientEvents} from '../constants';
 
 const socket : Socket<ClientToServerEvents, ServerToClientEvents> = io("ws://localhost:5050");
 
@@ -29,8 +12,25 @@ type input = {
   [key: number]: string;
 };
 
+// //Server to client side.
+// export interface ClientToServerEvents {
+//   broadcastClick : (res:{input:input, turn:string}) => void;
+//   broadcast_winEvent : (res:{winner: string}) => void;
+//   broadcast_resetEvent : (res:input)=>void
+//   hello : (res:{msg:string}) => void
+// }
 
-const initialState : input = {
+
+// //Client to server side.
+// export interface ServerToClientEvents {
+//   join_room_Event : (req : {roomNumber:number}) => void
+//   win_Event : (req:{winner:string, callback : ()=>void})=>void;
+//   clicked: (req:{input:input, turn:string}) => void;
+//   reset_Event : (req:input)=>void;
+// }
+
+
+const initialState : input | {} = {
   0: "",
   1: "",
   2: "",
@@ -50,9 +50,7 @@ function App() {
 
   const [turn, setTurn] = useState<string>("X");
   const [winner, setWinner] = useState<string | null>(null); 
-
-  //const socket = io("ws://localhost:5050")
-
+  const [roomNumber, setRoomNumber] = useState<number | undefined>(undefined);
 
   // Look for a winner.
   const CheckWin = ():void=>{
@@ -68,8 +66,9 @@ function App() {
         setWinner(input[win[0]]);
 
         //Create a req object to send.
-        let req_obj : {winner: string} = {
+        let req_obj : {winner: string, callback: ()=>void} = {
           winner: input[win[0]],
+          callback:()=>{console.log("hello")}
         }
         socket.emit("win_Event", req_obj);
         //return;
@@ -102,29 +101,68 @@ function App() {
 
 
   const handleReset = (e : React.MouseEvent<HTMLButtonElement, MouseEvent>) : void =>{
-    console.log("restting.")
-    setInput({...initialState})
+    setInput((prev:input)=>{
+      let newState : input = {};
+      Object.keys(prev).forEach((val)=>{
+        newState[Number(val)] = "";
+      });
+
+      return newState
+    })
+
     setTurn("X");
     setWinner(null);
+    //setInput({...initialState})
     socket.emit("reset_Event", initialState);
   }
 
-
+  // broadcast click
   const handleBroadCastClick = (res : any)=>{
     let {input, turn} = res;
     setInput({...input});
     setTurn(turn)
   }
 
+  // win event
   const handleBroadcastWin = (res:{winner:string})=>{
     setWinner(res.winner);
     setTurn("X");
   }
 
-  const handleBroadcasReset = (res:input)=>{
-    setInput({...res});
+  // reset event
+  const handleBroadcasReset = ()=>{
+    setInput((prev:input)=>{
+      let newState : input = {};
+      Object.keys(prev).forEach((val)=>{
+        newState[Number(val)] = "";
+      });
+
+      return newState
+    })
     setTurn("X");
     setWinner(null);
+  }
+
+
+  // join a selected room.
+  const joinRoom = (e:React.ChangeEvent<HTMLSelectElement>)=>{
+    const roomToJoin = Number((e.target as HTMLSelectElement).value);
+    setRoomNumber(roomToJoin);
+    console.log(roomToJoin)
+    socket.emit("join_room_Event", {roomNumber:roomToJoin})
+  }
+
+  const broadcastHello = (res:{msg:string})=>{
+    console.log(res.msg)
+  }
+  
+  const broadCastSocketToRoomConnected = (res:{msg:string, user_id:number})=>{
+    console.log(res)
+    console.log("User "+ res.user_id +" joined.")
+  }
+
+  const handleUserOverFlow = ()=>{
+    console.log("Can't connect to the given room as it is occupied.")
   }
 
   useEffect(()=>{
@@ -134,16 +172,31 @@ function App() {
 
     socket.on("broadcastClick", handleBroadCastClick);
     socket.on("broadcast_winEvent", handleBroadcastWin)
-    socket.on("broadcast_resetEvent", handleBroadcasReset)
+    socket.on("broadcast_resetEvent", handleBroadcasReset);
+    socket.on("test", broadcastHello);
+    socket.on("broadcast_socketToRoom_connected", broadCastSocketToRoomConnected);
+    socket.on("broadcast_user_overflow", handleUserOverFlow)
 
     return ()=>{
-      socket.off("broadcastClick", handleBroadCastClick)
+      socket.off("broadcastClick", handleBroadCastClick);
+      socket.off("broadcast_winEvent", handleBroadcastWin)
+      socket.off("broadcast_resetEvent", handleBroadcasReset);
+      socket.off("test", broadcastHello);
+      socket.off("broadcast_socketToRoom_connected", broadCastSocketToRoomConnected);
+      socket.off("broadcast_user_overflow", handleUserOverFlow)
     }
   },[])
 
 
   return (
     <div className="App">
+    <h3>Select a room to join...</h3>
+    <select onChange={joinRoom}>
+      <option value="" disabled selected>---</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
       <div className='btn-container'>
         <p className='winner'>{winner ? `${winner} wins!!` : "   "}</p>
         <div className='btn-wrapper'>
